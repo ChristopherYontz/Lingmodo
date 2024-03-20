befcf98cc0a0671a08a9427b37eae8ba3e9ba060
Christopher Yontz <yontzcay@gmail.com>
2024-03-19 23:42:45 -0700

    Styled log card components

--- linguameter-app/src/pages/DashboardPage.jsx
+++ linguameter-app/src/pages/DashboardPage.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { UserAuth } from "../contexts/AuthContext";
-import LogCard from "../components/LogCard";
+// import LogCard from "../components/LogCard";
 // import LogSubmissionForm from "../components/LogSubmissionForm";
 import pb from "../lib/pocketbase";
 
@@ -56,86 +56,187 @@ export default function DashboardPage() {
     //   </button>
     //   <LogSubmissionForm />
     // </div>
-    <div className="body-container h-lvh bg-gray-bg">
-      <div className="navbar-container h-lvh w-[16rem] top-0 left-0 bg-white fixed flex flex-col items-center">
-        <div className="navbar-top-container">
-          <img src="#" alt="Linguameter Logo" />
-          <h3>Linguameter</h3>
-        </div>
-        <div className="nav-options-container flex flex-col items-center">
-          <ul>
-            <li>Home</li>
-            <li>Community</li>
-            <li>Progress</li>
-            <li>Lists</li>
-            <li>Add Log</li>
-          </ul>
-        </div>
-        <div className="daily-goal-container">
-          <div className="daily-goal-top">
-            <h3>Daily goal</h3>
-            <span>Language</span>
-            <span>160m / 200m</span>
-          </div>
-          <div className="daily-goal-bottom">
-            <span>Graph</span>
-          </div>
-        </div>
-        <div className="navbar-bottom-container">
-          <span>Profile</span>
-        </div>
-      </div>
-      <div className="column-container flex flex-wrap flex-1">
-        <div className="navbar-padding w-[16rem]">
-          <span>Navbar padding</span>
-        </div>
-        <div className="column-1-2-container flex flex-wrap flex-1 space-x-8 mx-8 h-lvh">
-          <div className="column-1-container bg-white flex-1 rounded-[1rem]">
-            <div className="stats-container flex items-center flex-col">
-              <span>column 1</span>
-              <LogCard />
-            </div>
-          </div>
-          <div className="column-2-container bg-white flex-1 rounded-[1rem]">
-            <div className="posts-container flex items-center flex-col">
-              <span>column 2</span>
-              {userLogs.map((log) => {
-                return (
-                  <div
-                    key={log.id}
-                    className="card w-96 shadow-xl bg-green-primary mb-6"
-                  >
-                    <div className="card-body">
-                      <div className="flex flex-row justify-around items-center">
-                        <div className="flex flex-row">
-                          <div className="w-[2.688rem] h-[2.688rem] rounded-full">
-                            <img
-                              className="rounded-full"
-                              src={`http://127.0.0.1:8090/api/files/users/${log.expand.created_by.id}/${log.expand.created_by.avatar}`}
-                              alt="Profile Picture"
-                            />
-                          </div>
-                          <h2 className="card-title">
-                            @{log.expand.created_by.username}
-                          </h2>
-                        </div>
-                        <span>{log.date.slice(0, 10)}</span>
-                      </div>
-                      <div className="card w-full bg-white">
-                        <div className="card-body">
-                          <h2 className="card-title">{log.activity}</h2>
-                          <span>{log.duration}</span>
-                        </div>
-                      </div>
-                    </div>
+    // <div className="body-container h-lvh bg-gray-bg">
+    //   <div className="navbar-container h-lvh w-[16rem] top-0 left-0 bg-white fixed flex flex-col items-center">
+    //     <div className="navbar-top-container">
+    //       <img src="#" alt="Linguameter Logo" />
+    //       <h3>Linguameter</h3>
+    //     </div>
+    //     <div className="nav-options-container flex flex-col items-center">
+    //       <ul>
+    //         <li>Home</li>
+    //         <li>Community</li>
+    //         <li>Progress</li>
+    //         <li>Lists</li>
+    //         <li>Add Log</li>
+    //       </ul>
+    //     </div>
+    //     <div className="daily-goal-container">
+    //       <div className="daily-goal-top">
+    //         <h3>Daily goal</h3>
+    //         <span>Language</span>
+    //         <span>160m / 200m</span>
+    //       </div>
+    //       <div className="daily-goal-bottom">
+    //         <span>Graph</span>
+    //       </div>
+    //     </div>
+    //     <div className="navbar-bottom-container">
+    //       <span>Profile</span>
+    //     </div>
+    //   </div>
+    //   <div className="column-container flex flex-wrap flex-1">
+    //     <div className="navbar-padding w-[16rem]">
+    //       <span>Navbar padding</span>
+    //     </div>
+    //     <div className="column-1-2-container flex flex-wrap flex-1 space-x-8 mx-8 h-lvh">
+    //       <div className="column-1-container bg-white flex-1 rounded-[1rem]">
+    //         <div className="stats-container flex items-center flex-col">
+    //           <span>column 1</span>
+    //           <LogCard />
+    //         </div>
+    //       </div>
+    //       <div className="column-2-container bg-white flex-1 rounded-[1rem]">
+    //         <div className="posts-container flex items-center flex-col">
+    //           <span>column 2</span>
+    // {userLogs.map((log) => {
+    //   return (
+    //     <div
+    //       key={log.id}
+    //       className="card w-96 shadow-xl bg-green-primary mb-6"
+    //     >
+    //       <div className="card-body">
+    //         <div className="flex flex-row justify-around items-center">
+    //           <div className="flex flex-row">
+    //             <div className="w-[2.688rem] h-[2.688rem] rounded-full">
+    //               <img
+    //                 className="rounded-full"
+    //                 src={`http://127.0.0.1:8090/api/files/users/${log.expand.created_by.id}/${log.expand.created_by.avatar}`}
+    //                 alt="Profile Picture"
+    //               />
+    //             </div>
+    //             <h2 className="card-title">
+    //               @{log.expand.created_by.username}
+    //             </h2>
+    //           </div>
+    //           <span>{log.date.slice(0, 10)}</span>
+    //         </div>
+    //         <div className="card w-full bg-white">
+    //           <div className="card-body">
+    //             <h2 className="card-title">{log.activity}</h2>
+    //             <span>{log.duration}</span>
+    //           </div>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   );
+    // })}
+    //           <button className="btn" onClick={logout}>
+    //             Sign Out
+    //           </button>
+    //         </div>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+
+    // NEW LAYOUT
+    <div id="app" className="font-inter">
+      {/* {userLogs.map((log) => {
+        return (
+          <div
+            key={log.id}
+            className="card w-96 shadow-xl bg-green-primary mb-6"
+          >
+            <div className="card-body">
+              <div className="flex flex-row justify-around items-center">
+                <div className="flex flex-row">
+                  <div className="w-[2.688rem] h-[2.688rem] rounded-full">
+                    <img
+                      className="rounded-full"
+                      src={`http://127.0.0.1:8090/api/files/users/${log.expand.created_by.id}/${log.expand.created_by.avatar}`}
+                      alt="Profile Picture"
+                    />
                   </div>
-                );
-              })}
-              <button className="btn" onClick={logout}>
-                Sign Out
-              </button>
+                  <h2 className="card-title">
+                    @{log.expand.created_by.username}
+                  </h2>
+                </div>
+                <span>{log.date.slice(0, 10)}</span>
+              </div>
+              <div className="card w-full bg-white">
+                <div className="card-body">
+                  <h2 className="card-title">{log.activity}</h2>
+                  <span>{log.duration}</span>
+                </div>
+              </div>
             </div>
           </div>
+        );
+      })} */}
+      <div id="menu">cat</div>
+
+      <div id="layout">
+        <div className="spacer"></div>
+        <div className="content content-left">
+          {userLogs.map((log) => {
+            return (
+              <div key={log.id} className="card-tile">
+                <div className="card-tile-head bg-green-primary">
+                  <div className="profile">
+                    <img
+                      className="rounded-full"
+                      src={`http://127.0.0.1:8090/api/files/users/${log.expand.created_by.id}/${log.expand.created_by.avatar}`}
+                      alt="Profile Picture"
+                    />
+                  </div>
+                  <div>@{log.expand.created_by.username}</div>
+                  <div className="spacer"></div>
+                  <div>{log.date.slice(0, 10)}</div>
+                </div>
+
+                <div class="card-tile-body bg-white">
+                  <h2 className="card-title">{log.activity}</h2>
+                  <div className="grid grid-cols-3 justify-bewteen">
+                    <span className="text-start">{log.duration} minutes</span>
+                    <span className="text-center">{log.activity_type}</span>
+
+                    <span className="text-end">{log.language}</span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+        <div className="content content-right">
+          {userLogs.map((log) => {
+            return (
+              <div key={log.id} className="card-tile">
+                <div className="card-tile-head bg-green-primary">
+                  <div className="profile">
+                    <img
+                      className="rounded-full"
+                      src={`http://127.0.0.1:8090/api/files/users/${log.expand.created_by.id}/${log.expand.created_by.avatar}`}
+                      alt="Profile Picture"
+                    />
+                  </div>
+                  <div>@{log.expand.created_by.username}</div>
+                  <div className="spacer"></div>
+                  <div>{log.date.slice(0, 10)}</div>
+                </div>
+
+                <div class="card-tile-body bg-white">
+                  <h2 className="card-title text-black font-semibold">{log.activity}</h2>
+                  <div className="grid grid-cols-3 justify-bewteen">
+                    <span className="text-start">{log.duration} minutes</span>
+                    <span className="text-center">{log.activity_type}</span>
+
+                    <span className="text-end">{log.language}</span>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
--- linguameter-app/tailwind.config.js
+++ linguameter-app/tailwind.config.js
@@ -6,6 +6,17 @@ export default {
       colors: {
         'gray-bg': '#F3F2F7',
         'green-primary': '#93DC5C'
+      },
+      fontFamily: {
+        custom: ["Inter", "sans-serif"],
+      },
+      fontWeight: {
+        custom: {
+          light: 300,
+          normal: 400,
+          semibold: 600,
+          bold: 800,
+        }
       }
     },
   },
